d5a96bd017881c527c11ddfcc1fe666e98937ed8
dibbs0529 <kdibble0529@gmail.com>
2016-08-09 12:32:54 -0400

    NEEDS WORK!
    Needs turning on -60 and +60 axis.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float inputDel = 0.1f;
+    public float ForwVel = 12;
+    public float rotatVel = 100;
+
+    Quaternion targetRotation;
+    Rigidbody rBody;
+    float forwardInput, turnInput;
+
+    Quaternion TargetRotation
+    {
+
+        get { return targetRotation; }
+
+    }
+
+    void Start()
+    {
+
+        targetRotation = transform.rotation;
+        if (GetComponent<Rigidbody>())
+            rBody = GetComponent<Rigidbody>();
+        else
+            Debug.LogError("Char needs RIGIDBODY");
+
+
+    }
+
+
+    void GetInput()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+        turnInput = Input.GetAxis("Horiziontal");
+
+    }
+
+ 
+
+    void Update()
+    {
+        GetInput();
+        Turn(); 
+
+
+
+    }
+
+    void FixedUpdate()
+    {
+
+
+        Run(); 
+    }
+
+
+    void Run()
+
+    {
+        //Acceleration delay
+        if (Mathf.Abs(forwardInput) > inputDel)
+        {
+
+            //move 
+            rBody.velocity = transform.forward * forwardInput * ForwVel;
+
+
+        }
+
+        else
+            rBody.velocity = Vector3.zero; 
+
+    }
+
+
+    void Turn()
+    {
+        //Turning (HAS ERROR>?)
+        targetRotation *= Quaternion.AngleAxis(rotatVel * turnInput * Time.deltaTime, Vector3.up);
+        transform.rotation = targetRotation;
+
+
+    }
+
+}
